06acb39253628290252d534847e8968456ca6c4c
Matt Husted <mahusted@gmail.com>
2021-09-14 21:18:32 -0500

    Update readCSV.py
    Added comments and made code more universal

--- readCSV.py
+++ readCSV.py
@@ -1,5 +1,7 @@
 import csv
-with open('C:/Users/mahus/Documents/GitHub/python-off/mmsa-icu-beds.csv', 'r') as file:
+# To execute the following, download the mmsa-icu-beds.csv file to your local
+# machine and change the file path below to the directory where you copied it to
+with open('C:/Users/<UPDATE FILE PATH HERE>/mmsa-icu-beds.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
