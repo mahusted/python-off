b9f40330088b555a57de03bb717a3bba88d3e70f
Matt Husted <mahusted@gmail.com>
2021-09-14 21:14:40 -0500

    Changed dataset

--- readCSV.py
+++ readCSV.py
@@ -1,5 +1,5 @@
 import csv
-with open('C:/Users/mahus/Dropbox/Python/ReadCSV/csv-1.csv', 'r') as file:
+with open('C:/Users/mahus/Documents/GitHub/python-off/mmsa-icu-beds.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
